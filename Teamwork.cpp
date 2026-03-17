46f22bc8ce8e29cf4b53c391cc114ad3140b64fb
javicede <118625272+javicede@users.noreply.github.com>
2026-03-17 14:20:26 +0100

    Implement SumArray function for array summation
    añadí uno de los dos métodos a pedir

--- Teamwork.cpp
+++ Teamwork.cpp
@@ -36,6 +36,28 @@ void AsmAccess() {
 		exit(0);
 	}
 }
+void SumArray() {
+	int tamano = 3;
+	unsigned char* ID = new unsigned char[tamano]; // nos aseguramos que cada valor esté entre 0 y 255
+	// Lee los valores de la terminal y se colocan en el vector
+	cout << "Introduzca los tres elementos del vector (0-255): " << endl;
+	for (int i = 0; i < tamano; i++) {
+		int temp; // aseguramos que guarde el valor entero
+		cin >> temp;
+		ID[i] = (unsigned char)temp; // se guarda dicho valor dentro de los 8  bits
+	}
+	int suma = ID[0] + ID[1] + ID[2];
+	int final = (ID[1] + 1) * 30;
+	if (suma > final) {
+		cout << "El valor de la suma final es: " << suma << endl;
+	}
+	else {
+		cout << "Fallo en la suma." << endl;
+		delete[] ID; // se elimina el vector para ahorrar memoria. Se usa cuando utilizas 'new'.
+		exit(0);
+	}
+	delete[] ID; // caso donde sí se cumple, se sigue borrando para ahorrar memoria después de haber hecho el procedimiento
+}
 
 int main()
 {
