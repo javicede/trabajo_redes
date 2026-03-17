e2cfc77a448fbe98ec9ee4c7f43606ec4387ff51
javicede <118625272+javicede@users.noreply.github.com>
2026-03-17 15:02:09 +0100

    Parte javi
    Ya se han añadido ambos métodos de la parte de Javi, tuve que añadir el vector 'ID' para comparar valores, si encuentra otro método para hacerlo, notifíquelo

--- Teamwork.cpp
+++ Teamwork.cpp
@@ -36,6 +36,7 @@ void AsmAccess() {
 		exit(0);
 	}
 }
+
 void SumArray() {
 	int tamano = 3;
 	unsigned char* ID = new unsigned char[tamano]; // nos aseguramos que cada valor esté entre 0 y 255
@@ -48,15 +49,50 @@ void SumArray() {
 	}
 	int suma = ID[0] + ID[1] + ID[2];
 	int final = (ID[1] + 1) * 30;
-	if (suma > final) {
-		cout << "El valor de la suma final es: " << suma << endl;
-	}
-	else {
+	if (suma < final) {
 		cout << "Fallo en la suma." << endl;
 		delete[] ID; // se elimina el vector para ahorrar memoria. Se usa cuando utilizas 'new'.
 		exit(0);
 	}
 	delete[] ID; // caso donde sí se cumple, se sigue borrando para ahorrar memoria después de haber hecho el procedimiento
+}
+
+void ControlBitParity(unsigned char* ID) {
+	unsigned int num1;
+	unsigned int num2;
+	cout << "Introduzca los dos números enteros sin signo: ";
+	cin >> num1;
+	cin >> num2;
+	// Aislamos los bits que se han usado en las posiciones guardadas de ID[3] y ID[4]
+	int bitA = (num1 >> ID[3]) & 0x00000001;
+	int bitB = (num2 >> ID[4]) & 0x00000001;
+
+	if (bitA != bitB){
+		cout << "Entrada incorrecta." << endl;
+		delete[] ID;
+		exit(0);
+	}
+	int contador_unos1 = 0;
+	// contador de 1s entre ID[3] y ID[4]
+	for (int i = ID[2]; i <= ID[4]; i++) {
+		if ((num1 >> i) & 1) {
+			contador_unos1++;
+		}
+	}
+	int contador_unos2 = 0;
+	// contador de 1s entre ID[1] y ID[3]
+	for (int i = ID[1]; i <= ID[3]; i++) {
+		if ((num2 >> i) & 1) {
+			contador_unos2++;
+		}
+	}
+	// suma de ambos contadores, condición de que NO se cumpla
+	if ((contador_unos1 + contador_unos2) % 2 != 0) {
+		cout << "Fallo" << endl;
+		delete[] ID;
+		exit(0);
+	}
+	delete[] ID;
 }
 
 int main()
